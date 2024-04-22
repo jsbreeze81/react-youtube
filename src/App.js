476754af884c85b4e75ba8d24b65eb73276154ed
jsbreeze81 <jsalla@nate.com>
2024-04-22 20:26:09 +0900

    페이지만들기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./page/Home";
+import Today from "./page/Today";
+import Developer from "./page/Developer";
+import Webd from "./page/Webd";
+import Website from "./page/Website";
+import Gsap from "./page/Gsap";
+import Port from "./page/Port";
+import Youtube from "./page/Youtube";
+import Channel from "./page/Channel";
+import Video from "./page/Video";
+import Search from "./page/Search";
+import Not from "./page/Not";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/today" element={<Today />} />
+          <Route path="/developer" element={<Developer />} />
+          <Route path="/webd" element={<Webd />} />
+          <Route path="/website" element={<Website />} />
+          <Route path="/gsap" element={<Gsap />} />
+          <Route path="/port" element={<Port />} />
+          <Route path="/youtube" element={<Youtube />} />
+          <Route path="/channel/:channelID" element={<Channel />} />
+          <Route path="/video/:videoID" element={<Video />} />
+          <Route path="/search/:searchID" element={<Search />} />
+          <Route path="/*" element={<Not />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/page/Gsap.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Gsap() {
+  return <div>Gsap</div>;
+}
+
+export default Gsap;
